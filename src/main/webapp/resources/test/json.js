a7cdce419136552ae3540ef54e3b8bd9ddbd5d3f
minjae0413 <140949456+minjae0413@users.noreply.github.com>
2023-08-08 22:08:17 +0900

    spring project push
    spring project push

---
+++ src/main/webapp/resources/test/json.js
@@ -0,0 +1,122 @@
+var jsonData = [
+    {
+      "_id": "609e126c4466fd7f5205f959",
+      "age": 27,
+      "eyeColor": "brown",
+      "name": "Davidson Warren"
+    },
+    {
+      "_id": "609e126c2cf17a1bbd7da909",
+      "age": 32,
+      "eyeColor": "green",
+      "name": "Goff Mcclain"
+    },
+    {
+      "_id": "609e126ceac10471135c6c62",
+      "age": 24,
+      "eyeColor": "blue",
+      "name": "Blevins Hale"
+    },
+    {
+      "_id": "609e126c8b21c2f3d198434d",
+      "age": 23,
+      "eyeColor": "brown",
+      "name": "Dunlap Marshall"
+    },
+    {
+      "_id": "609e126c09ec019a91033e1e",
+      "age": 23,
+      "eyeColor": "blue",
+      "name": "Deann Morales"
+    },
+    {
+      "_id": "609e126c294c43c190b39a71",
+      "age": 30,
+      "eyeColor": "green",
+      "name": "Ayers Roach"
+    },
+    {
+      "_id": "609e126c06db05020d0e5672",
+      "age": 20,
+      "eyeColor": "brown",
+      "name": "Lina Carey"
+    },
+    {
+      "_id": "609e126c045a03d5597518b6",
+      "age": 31,
+      "eyeColor": "green",
+      "name": "Underwood Mathis"
+    },
+    {
+      "_id": "609e126cc40018dff1ffe3aa",
+      "age": 31,
+      "eyeColor": "green",
+      "name": "Vang French"
+    },
+    {
+      "_id": "609e126ce9429dc6eabc812e",
+      "age": 36,
+      "eyeColor": "blue",
+      "name": "Erika Summers"
+    },
+    {
+      "_id": "609e126c5bd449fab7f50cac",
+      "age": 26,
+      "eyeColor": "blue",
+      "name": "Houston Silva"
+    },
+    {
+      "_id": "609e126cdbc8223aaffca544",
+      "age": 38,
+      "eyeColor": "brown",
+      "name": "Neal James"
+    },
+    {
+      "_id": "609e126c57e98383cee0d43c",
+      "age": 31,
+      "eyeColor": "blue",
+      "name": "Malinda Franco"
+    },
+    {
+      "_id": "609e126c82165411a2b26932",
+      "age": 26,
+      "eyeColor": "brown",
+      "name": "Stevens Pennington"
+    },
+    {
+      "_id": "609e126c4b321eed1866bee9",
+      "age": 40,
+      "eyeColor": "green",
+      "name": "Hester Goodman"
+    },
+    {
+      "_id": "609e126c65f25a0d44d1c7c0",
+      "age": 29,
+      "eyeColor": "blue",
+      "name": "Nicholson Savage"
+    },
+    {
+      "_id": "609e126c6a52f455656db43c",
+      "age": 35,
+      "eyeColor": "brown",
+      "name": "Amparo Campbell"
+    },
+    {
+      "_id": "609e126ca9c031298d6c9795",
+      "age": 32,
+      "eyeColor": "green",
+      "name": "Noreen Maddox"
+    },
+    {
+      "_id": "609e126c132e111a5e58cd77",
+      "age": 35,
+      "eyeColor": "brown",
+      "name": "Fleming Mcguire"
+    },
+    {
+      "_id": "609e126cdf76ddd81697b5c6",
+      "age": 38,
+      "eyeColor": "blue",
+      "name": "Wilkerson Hoover"
+    }
+  ]
